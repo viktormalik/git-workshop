b295018e62ec4a6c339e2b99f4c220b210a4277e
Petr Stodulka <xstodu05@gmail.com>
2020-03-29 20:46:36 +0200

    Improve processing of the cmdline parameters
    This is preparation for the new functionality to be able to add
    additional options instead of the counter ones. The processing
    of the cmdline has been separated into the process_cmdline function.
    The configuration is now represented by the Configuration struct.
    
    Still it is possible to use none or just one counter and parameters
    is positional.

--- wc.c
+++ wc.c
@@ -8,8 +8,11 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
+
 
 void print_help() {
     printf("wc <filename> [-c | -l | -w | -s] [sep]\n");
@@ -23,41 +26,86 @@ void print_help() {
 }
 
 
-int main(int argc, char *argv[]) {
+struct Configuration {
+    bool char_counter;    // character counter
+    bool line_counter;    // line counter
+    bool word_counter;    // word counter
+
+    char* separator;      // define own separator for counting
+    char* filename;       // the file that should be parsed
+} config = {false, false, false, NULL};
+
+
+int process_cmdline(int argc, char *argv[]) {
     if (argc == 2 && strcmp("-h", argv[1]) == 0) {
         print_help();
-        return 0;
+        exit(0);
     }
 
-    if (!(argc == 3 || (argc == 4 && strcmp("-s", argv[2]) == 0))) {
+    if (argc < 3) {
         fprintf(stderr, "Incorrect number of arguments\n");
         return 1;
     }
-    FILE *f = fopen(argv[1], "r");
 
-    // character counter
-    int c_opt = strcmp("-c", argv[2]) == 0;
+    int count_defined = 0;
+    for (int i=2; i < argc; i++) {
+        if (strcmp("-c", argv[i]) == 0) {
+            config.char_counter = true;
+            count_defined++;
+            continue;
+        } else if (strcmp("-w", argv[i]) == 0) {
+            config.word_counter = true;
+            count_defined++;
+            continue;
+        } else if (strcmp("-l", argv[i]) == 0) {
+            config.line_counter = true;
+            count_defined++;
+            continue;
+        } else if (strcmp("-s", argv[i]) == 0) {
+            if (i+1 >= argc) {
+              fprintf(stderr, "The '-s' option requires argument.\n");
+              return 1;
+            }
+            i++;
+            config.separator = argv[i];
+            count_defined++;
+            continue;
+        } else if (strcmp("-h", argv[i]) == 0) {
+            print_help();
+            exit(0);
+        }
+    }
 
-    // line counter
-    int l_opt = strcmp("-l", argv[2]) == 0;
+    config.filename = argv[1];
 
-    // word counter
-    int w_opt = strcmp("-w", argv[2]) == 0;
+    if (count_defined != 1) {
+      fprintf(stderr, "Only one counter type is allowed in the same time.\n");
+      return 1;
+    }
 
-    // counter using the given separator
-    int s_opt = strcmp("-s", argv[2]) == 0;
+    return 0;
+}
 
-    if (c_opt || l_opt || w_opt || s_opt) {
+int main(int argc, char *argv[]) {
+
+    if (process_cmdline(argc, argv))
+        return 1;
+
+    FILE *f = fopen(config.filename, "r");
+
+    // do nothing if no counter is defined
+    if (config.char_counter || config.word_counter || config.line_counter
+            || config.separator != NULL) {
         int i = 0;
         int c;
         while ((c = fgetc(f)) != EOF) {
-            if (l_opt && c == '\n') {
+            if (config.line_counter && c == '\n') {
                 i++;
-            } else if (c_opt) {
+            } else if (config.char_counter) {
                 i++;
-            } else if (w_opt && isspace(c)) {
+            } else if (config.word_counter && isspace(c)) {
                 i++;
-            } else if (s_opt && c == argv[3][0]) {
+            } else if (config.separator && c == config.separator[0]) {
                 i++;
             }
         }
