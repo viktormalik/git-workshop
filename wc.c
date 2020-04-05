e2d4c2eb299b799357d6bcd732de56c7b18fedbf
Petr Stodulka <xstodu05@gmail.com>
2020-04-05 18:54:38 +0200

    cmdline parsing: filename is not positional anymore
    Originally the input filename had to be given as the first parameter
    on the cmdline. The position is not required now. So the filename
    can be specified e.g. on the last position.

--- wc.c
+++ wc.c
@@ -48,7 +48,7 @@ int process_cmdline(int argc, char *argv[]) {
     }
 
     int count_defined = 0;
-    for (int i=2; i < argc; i++) {
+    for (int i = 1; i < argc; i++) {
         if (strcmp("-c", argv[i]) == 0) {
             config.char_counter = true;
             count_defined++;
